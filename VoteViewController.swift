c62b683dd6d284ce6e7a5b15dd93ca2cc742b993
Jing Sun <bigheadhenry@gmail.com>
2016-01-17 11:07:53 +0800

    增加alertController弹出显示结果
    用alertController弹出警告框，显示投票结果，并包含投票成功或失败的提示，点击重新投票后归零。

--- VoteViewController.swift
+++ VoteViewController.swift
@@ -9,26 +9,55 @@
 import UIKit
 
 class VoteViewController: UIViewController {
-  
+
   var agree = 0
-  
   var disagree = 0
+  
+  func alertView(success: Bool) -> () {
+    let votepass = UIAlertController(title: "投票结果", message: "\(agree)票赞成，\(disagree)票反对，投票通过！", preferredStyle: UIAlertControllerStyle.Alert)
+    votepass.addAction(UIAlertAction(
+      title: "重新投票",
+      style: .Cancel)
+      { (action: UIAlertAction) -> Void in
+        self.agree = 0
+        self.disagree = 0
+      }
+    )
+    
+    let votedispass = UIAlertController(title: "投票结果", message: "\(agree)票赞成，\(disagree)票反对，投票未通过！", preferredStyle: UIAlertControllerStyle.Alert)
+      votedispass.addAction(UIAlertAction(
+      title: "重新投票",
+      style: .Cancel)
+      { (action: UIAlertAction) -> Void in
+        self.agree = 0
+        self.disagree = 0
+      }
+    )
+        
+    if success == true {
+      presentViewController(votepass, animated: true, completion: nil)
+    } else {
+      presentViewController(votedispass, animated: true, completion: nil)
+    }
+  }
   
   @IBAction func Agree(sender: UIButton) {
     agree++
-    print("\(agree) agree")
+    print("\(agree + disagree) vote,\(agree) agree,\(disagree) disagree")
     if agree + disagree >= 6 {
       print("Vote Over，\(agree)agrees,\(disagree)disagrees")
+    alertView(true)
     }
   }
 
   @IBAction func Disagree(sender: UIButton) {
     disagree++
-    print("\(disagree) disagree")
+    print("\(agree + disagree) vote,\(agree) agree,\(disagree) disagree")
     if agree + disagree >= 6 {
       print("Vote Over，\(agree)agrees,\(disagree)disagrees")
+      alertView(false)
+
     }
 
   }
-
 }
